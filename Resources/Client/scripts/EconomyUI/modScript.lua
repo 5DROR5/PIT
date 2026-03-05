e2efb5bccc754d82cec8c27f84aa0b887715d0cf
5DROR5 <5dror55@gmail.com>
2026-03-05 18:06:12 +0200

    Create modScript.lua

---
+++ Resources/Client/scripts/EconomyUI/modScript.lua
@@ -0,0 +1,10 @@
+log('I', "[EconomyUI] modScript", "modScript.lua LOADED")
+
+load("key")
+setExtensionUnloadMode("key", "manual")
+
+load("minimap")
+setExtensionUnloadMode("minimap", "manual")
+
+print("[EconomyUI] modScript.lua LOADED")
+
