1fe4406d6e9825e4d9354fe39dd99762f92551e9
5DROR5 <5dror55@gmail.com>
2026-03-06 11:25:33 +0200

    Create modScript.lua

---
+++ Resources/Client/UIMPI.zip/scripts/perf-ui/modScript.lua
@@ -0,0 +1,7 @@
+log('I', "[performanceLimiter] modScript", "modScript.lua LOADED")
+
+load("performanceLimiter")
+setExtensionUnloadMode("performanceLimiter", "manual")
+
+print("[performanceLimiter] modScript.lua LOADED")
+
